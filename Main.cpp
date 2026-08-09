43e9bad93e83cb51644b9dc3c4bd962e52a73f65
Eetu Oinonen <eetu.oinonen04@gmail.com>
2026-08-09 13:16:48 +0300

    added a dirt block texture

--- Main.cpp
+++ Main.cpp
@@ -6,8 +6,8 @@
 
 #include <vector>
 
-// #define STB_IMAGE_IMPLEMENTATION
-// #include "stb_image.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #include "shader.h"
 #include "Window.h"
@@ -26,7 +26,7 @@ int main()
     Window window = Window("Mineclone", 800, 600);
     if (!window.isValid()) return -1;
 
-    Shader ourShader = Shader("assets/block.vs", "assets/block.fs");
+    Shader ourShader = Shader("assets/block.vert", "assets/block.frag");
 
     unsigned int VAO, VBO;
     glGenVertexArrays(1, &VAO);
@@ -37,11 +37,17 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), static_cast<void*>(nullptr));
+    // Vertex positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), static_cast<void*>(nullptr));
     glEnableVertexAttribArray(0);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+    // Color information
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    // Texture positions
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), reinterpret_cast<void*>(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glEnable(GL_DEPTH_TEST);
 
@@ -51,6 +57,34 @@ int main()
         glm::vec3( 1.0f,  1.0f,  0.0f),
         glm::vec3( 1.0f,  2.0f,  0.0f),
     };
+
+
+    // ======== TEXTURE STUFF =========== MAYBE MAKE A TEXTURE CLASS?
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("assets/dirt.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // Binding the texture
+    glBindTexture(GL_TEXTURE_2D, texture);
+
 
     while(!window.shouldClose())
     {
